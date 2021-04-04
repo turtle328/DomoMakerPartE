7952ce44e1f112d83ce67f5f316b2f8535a11101
Alex Rosenbach <ajr6974@rit.edu>
2021-04-04 00:29:38 -0400

    Finished

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
+const session = require('express-session');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -32,6 +33,7 @@ app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ key: 'sessionid', secret: 'Domo Arigato', resave: true, saveUninitialized: true }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
